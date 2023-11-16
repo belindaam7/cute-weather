1321e4d69dc8c3eb78060259031d8a3da7254de9
belindaam7 <bsamezquita@gmail.com>
2023-11-15 23:25:34 -0800

    Time and date configuration

--- src/java.js
+++ src/java.js
@@ -4,13 +4,13 @@ function search(event) {
   let city = searchCity.value;
   let cityElement = document.querySelector(`#current-city`);
   let apiKey = `bec049cdcofb5t08d94f2fc0c3440fa3`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city.value}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(updateTemp);
-  cityElement.innerHTML = city;
+  cityElement.innerHTML = city.toUpperCase();
 }
 function updateTemp(response) {
-  let currentTemp = document.querySelector(`.current-temp`);
-  currentTemp.innerHTML = response.data.temperature.current;
+  let currentTemp = document.querySelector(`#current-temp`);
+  currentTemp.innerHTML = Math.round(response.data.temperature.current);
 }
 let searchButton = document.querySelector(`.search-city-button`);
 searchButton.addEventListener(`click`, search);
