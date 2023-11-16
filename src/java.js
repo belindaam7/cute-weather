0f912eb00a05e817345952754a6899ba4f9be9ca
belindaam7 <bsamezquita@gmail.com>
2023-11-15 22:25:45 -0800

    Added API

--- src/java.js
+++ src/java.js
@@ -1,18 +1,16 @@
-function updateTemp(response) {
-  response.preventDefault();
-  let currentTemp = document.querySelector(`.current-temp`);
-  currentTemp.innerHTML = response.data.temperature.current;
-}
-
 function search(event) {
   event.preventDefault();
   let searchCity = document.querySelector(`#form-search-city`);
   let city = searchCity.value;
   let cityElement = document.querySelector(`#current-city`);
-  cityElement.innerHTML = city;
   let apiKey = `bec049cdcofb5t08d94f2fc0c3440fa3`;
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city.value}&key=${apiKey}`;
   axios.get(apiUrl).then(updateTemp);
+  cityElement.innerHTML = city;
+}
+function updateTemp(response) {
+  let currentTemp = document.querySelector(`.current-temp`);
+  currentTemp.innerHTML = response.data.temperature.current;
 }
 let searchButton = document.querySelector(`.search-city-button`);
 searchButton.addEventListener(`click`, search);
